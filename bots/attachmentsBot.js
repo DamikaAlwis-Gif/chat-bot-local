27573daed979b2f0148566162482e64b3d53f558
DamikaAlwis-Gif <damikaa@cse.mrt.ac.lk>
2024-04-08 14:38:18 +0530

    add more functionality and fucntion for summarize responce

--- bots/attachmentsBot.js
+++ bots/attachmentsBot.js
@@ -1,138 +1,180 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, ActivityTypes } = require("botbuilder");
-const path = require("path");
+const {
+  TurnContext,
+  MessageFactory,
+  TeamsActivityHandler,
+} = require("botbuilder");
+
 const axios = require("axios");
-const fs = require("fs");
 
-class AttachmentsBot extends ActivityHandler {
-    constructor() {
-        super();
+class AttachmentsBot extends TeamsActivityHandler {
+  constructor() {
+    super();
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    // If the Activity is a ConversationUpdate, send a greeting message to the user.
-                    await context.sendActivity(
-                        "Welcome to the Attachment Handling sample! Send me an attachment and I will save it."
-                    );
-                    await context.sendActivity(
-                        "Alternatively, I can send you an attachment."
-                    );
-                    await next();
-                }
-            }
-        });
+    this.imageAnalysisResponce = null;
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        if (membersAdded[cnt].id !== context.activity.recipient.id) {
+          // send a greeting message to the user.
+          await context.sendActivity(
+            "Welcome to the AI_bot! I'm here to assist you. Please upload an image."
+          );
 
-        this.onMessage(async (context, next) => {
-            // Determine how the bot should process the message by checking for attachments.
-            if (
-                context.activity.attachments &&
-                context.activity.attachments.length > 0
-            ) {
-                // The user sent an attachment and the bot should handle the incoming attachment.
-                await this.handleIncomingAttachment(context);
-            } else {
-                // Since no attachment was received, send a message indicating that only incoming attachments are handled.
-                await context.sendActivity(
-                    "I only handle incoming attachments. Please upload an image."
-                );
-                await next();
-            }
-        });
-    }
-
-    /**
-     * Saves incoming attachments to disk by calling `this.downloadAttachmentAndWrite()` and
-     * responds to the user with information about the saved attachment or an error.
-     * @param {Object} turnContext
-     */
-    async handleIncomingAttachment(turnContext) {
-        // Prepare Promises to download each attachment and then execute each Promise.
-        const promises = turnContext.activity.attachments.map(
-            this.downloadAttachmentAndWrite
-        );
-        const successfulSaves = await Promise.all(promises);
-        console.log(successfulSaves)
-
-        // Replies back to the user with information about where the attachment is stored on the bot's server,
-        // and what the name of the saved file is.
-        async function replyForReceivedAttachments(localAttachmentData) {
-            if (localAttachmentData) {
-                // Because the TurnContext was bound to this function, the bot can call
-                // `TurnContext.sendActivity` via `this.sendActivity`;
-                await turnContext.sendActivity(
-                    // `Attachment "${localAttachmentData.fileName}" ` +
-                    //     `has been received and saved to "${localAttachmentData.localPath}".`
-                    `This is info: ${localAttachmentData.result.text}`
-                );
-            } else {
-                await turnContext.sendActivity(
-                    "Attachment was not successfully saved to disk."
-                );
-            }
+          await next();
         }
+      }
+    });
 
-        // Prepare Promises to reply to the user with information about saved attachments.
-        // The current TurnContext is bound so `replyForReceivedAttachments` can also send replies.
-        const replyPromises = successfulSaves.map(replyForReceivedAttachments);
-        await Promise.all(replyPromises);
-    }
+    this.onMessage(async (context, next) => {
+      // Determine how the bot should process the message by checking for attachments.
+      const imageExtensions = ["jpeg", "jpg", "png"];
+      const imageRegex = /image\/.*/;
+      TurnContext.removeRecipientMention(context.activity);
+      const attachments = context.activity.attachments;
 
-    /**
-     * Downloads attachment to the disk.
-     * @param {Object} attachment
-     */
-    async downloadAttachmentAndWrite(attachment) {
-        // Retrieve the attachment via the attachment's contentUrl.
-        const url = attachment.contentUrl;
-        console.log(url)
+      if (context.activity.text) {
+        // await context.sendActivity(`Echo: ${context.activity.text}`);
+        const messageText = context.activity.text.toLowerCase();
+        if (messageText.includes("more")) {
+          // Provide additional details
 
-        try {
-
-            // arraybuffer is necessary for images
-            const response = await axios.get(url, {
-                responseType: "arraybuffer",
-            });
-            //console.log(response)
-
-            // If user uploads JSON file, this prevents it from being written as "{"type":"Buffer","data":[123,13,10,32,32,34,108..."
-            if (response.headers["content-type"] === "application/json") {
-                response.data = JSON.parse(response.data, (key, value) => {
-                    return value && value.type === "Buffer"
-                        ? Buffer.from(value.data)
-                        : value;
-                });
-            }
-            
-            console.log(response.data);
-            const res = await axios.post(
-                "http://localhost:5000/api/imageanalysis/imagebuffer",
-                {
-                    // imageUrl:
-                    //     "https://th.bing.com/th/id/R.3d53a122167671f14955623753d586e8?rik=mHNc3MppvzbHmQ&pid=ImgRaw&r=0",
-
-                    imageBuffer:response.data
-                }
+          if (this.imageAnalysisResponce) {
+            const summary = this.summarizeResponse(
+              this.imageAnalysisResponce.result
             );
-            
-            return res.data;
-
-
-        } catch (error) {
-            //console.error(error);
-            return undefined;
+            await context.sendActivity(summary);
+          } else {
+            await context.sendActivity(
+              "Please upload a image first to get details of a image"
+            );
+          }
+        } else {
+          await context.sendActivity(
+            "Sorry, I can't assist with that at the moment. Please upload an image."
+          );
         }
-        // If no error was thrown while writing to disk, return the attachment's name
-        // and localFilePath for the response back to the user.
-        // return {
-        //     // fileName: attachment.name,
-        //     // localPath: localFileName,
-            
-        // };
+      } else if (attachments && attachments[0]) {
+        // if (
+        //     attachments[0].contentType === "application/vnd.microsoft.teams.file.download.info" &&
+        //     imageExtensions.includes(attachments[0].content.fileType)
+        // ) {
+
+        //     await this.handleIncomingAttachment(context);
+
+        // } else if (imageRegex.test(attachments[0].contentType)) {
+        //     await context.sendActivity(
+        //         "I'm sorry, I can't process inline images."
+        //     );
+        // } else {
+        //     await context.sendActivity(
+        //         "I'm sorry, I can only process incoming images."
+        //     );
+        // }
+
+        await this.handleIncomingAttachment(context);
+      } else {
+        await context.sendActivity(
+          "I'm sorry, I can only handle incoming attachments or text messages. Please upload an image or send a text message."
+        );
+      }
+
+      await next();
+    });
+  }
+
+  summarizeResponse = (result) => {
+    // extracting the caption details
+    const caption = result.captionResult.text;
+    const captionConfidence = result.captionResult.confidence;
+
+    // Extracting dense captions
+    const denseCaptions = result.denseCaptionsResult.values.map(
+      (caption) => caption.text
+    );
+    // Extracting tags
+    const tags = result.tagsResult.values.map((tag) => tag.name);
+
+    // extracting
+    // Extracting objects
+    const objectTags = result.objectsResult.values.flatMap((object) =>
+      object.tags.map((tag) => tag.name)
+    );
+
+    // Extracting image credit
+    const credit = result.readResult.blocks
+      .filter((block) =>
+        block.lines.some((line) =>
+          line.words.some((word) => word.text === "Credit:")
+        )
+      )
+      .map((block) =>
+        block.lines
+          .find((line) => line.words.some((word) => word.text === "Credit:"))
+          .words.filter((word) => word.text !== "Credit:")
+          .map((word) => word.text)
+      )
+      .join(" ");
+
+    // Summarizing the content
+    const summary = `
+    Caption: ${caption} (Confidence: ${captionConfidence})
+    Dense Captions: ${denseCaptions.join(", ")}
+    Tags: ${tags.join(", ")}
+    Object Tags: ${objectTags.join(", ")}
+    Image Credit: ${credit}`;
+    return summary;
+  };
+
+  handleIncomingAttachment = async (context) => {
+    const res = await this.getImageAnalysisResponce(context);
+    if (res) {
+      this.imageAnalysisResponce = res;
+      const caption = res.result.captionResult.text;
+      await context.sendActivity(`This image contains: ${caption}`);
+    } else {
+      await context.sendActivity(
+        "An error occurred while processing the image."
+      );
     }
+  };
+
+  getImageAnalysisResponce = async (context) => {
+    // Retrieve the attachment via the attachment's contentUrl.
+    const url = context.activity.attachments[0].contentUrl;
+    // const url = context.activity.attachments[0].content.downloadUrl;
+
+    try {
+      // arraybuffer is necessary for images
+      const response = await axios.get(url, {
+        responseType: "arraybuffer",
+      });
+
+      // If user uploads JSON file, this prevents it from being written as "{"type":"Buffer","data":[123,13,10,32,32,34,108..."
+      if (response.headers["content-type"] === "application/json") {
+        response.data = JSON.parse(response.data, (key, value) => {
+          return value && value.type === "Buffer"
+            ? Buffer.from(value.data)
+            : value;
+        });
+      }
+
+      const res = await axios.post(
+        // "https://image-analyze-api.azurewebsites.net/api/imageanalysis/imagebuffer",
+        "http://localhost:5000/api/imageanalysis/imagebuffer",
+
+        {
+          imageBuffer: response.data,
+        }
+      );
+
+      return res.data;
+    } catch (error) {
+      return undefined;
+    }
+  };
 }
 
 module.exports.AttachmentsBot = AttachmentsBot;
